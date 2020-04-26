ea539c525abd77a43a32754174c092ef15400f4b
PengFei <feifei.feixia@163.com>
2020-04-26 10:22:28 +0800

    startapp blog db

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField(max_length=100)
+    created_time = models.DateTimeField
+    modified_time = models.DateTimeField
+    # 文章摘要
+    excerpt = models.CharField(max_length=100, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
