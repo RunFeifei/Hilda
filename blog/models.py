9267e0b504944b2dfa8f5f81499f572a61ae8bb4
PengFei <feifei.feixia@163.com>
2020-04-27 13:06:20 +0800

    toChinese01

--- blog/models.py
+++ blog/models.py
@@ -7,12 +7,33 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name = '分类'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name = '标签'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
+    class Meta:
+        verbose_name = '文章'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.title
+
     title = models.CharField(max_length=100)
     body = models.TextField(max_length=100)
     created_time = models.DateTimeField()
