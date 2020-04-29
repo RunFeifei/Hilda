74a18cc7f6768231be9347f7e26a7301eed59b1c
PengFei <feifei.feixia@163.com>
2020-04-29 11:03:57 +0800

    list comments

--- comments/templatetags/comment_extra.py
+++ comments/templatetags/comment_extra.py
@@ -13,3 +13,15 @@ def show_comment_form(context, post, form=None):
         'form': form,
         'post': post,
     }
+
+
+@register.inclusion_tag('comments/inclusions/_list.html', takes_context=True)
+def show_comments(context, post):
+    # 等价于 Comment.objects.all().filter(post=post)
+    # 例如 Post.objects.filter(category=cate) 等价 cate.post_set.all()
+    comment_list = post.comment_set.all().order_by('-created_time')
+    comment_count = comment_list.count()
+    return {
+        'comment_count': comment_count,
+        'comment_list': comment_list,
+    }
