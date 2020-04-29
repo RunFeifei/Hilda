567045a3b8c996bb169c4072806489a290fdc147
PengFei <feifei.feixia@163.com>
2020-04-29 12:53:19 +0800

    init polls

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('post/<int:pk>/', views.detail, name='detail'),
-    path('post/<int:year>/<int:month>/', views.archives, name='archive'),
-    path('categories/<int:pk>/', views.category, name='category'),
-    path('tags/<int:pk>/', views.tag, name='tag'),
+    path('blog', views.index, name='index'),
+    path('blog/post/<int:pk>/', views.detail, name='detail'),
+    path('blog/post/<int:year>/<int:month>/', views.archives, name='archive'),
+    path('blog/categories/<int:pk>/', views.category, name='category'),
+    path('blog/tags/<int:pk>/', views.tag, name='tag'),
 ]
