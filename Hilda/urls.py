f1688f1b63c15f8745f7068c9e60ab05afd78f47
PengFei <feifei.feixia@163.com>
2020-05-07 11:22:19 +0800

    init api module

--- Hilda/urls.py
+++ Hilda/urls.py
@@ -13,13 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
 
+router = routers.DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('', include('comments.urls')),
+    path('api/', include(router.urls)),
+    path('api/auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
